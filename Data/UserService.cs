4b4f0eb8242192ea3f20db6fa66d2f6ed822a65f
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2023-03-20 22:49:19 +0100

    add private chat

--- Data/UserService.cs
+++ Data/UserService.cs
@@ -3,6 +3,7 @@ namespace BlazorChat.Data;
 public class UserService
 {
     private Dictionary<string, string> _users = new Dictionary<string, string>();
+
     public void Add(string connectionId, string username)
     {
         _users[username] = connectionId;
@@ -13,14 +14,19 @@ public class UserService
         _users.Remove(username);
     }
 
-    public string? GetConnectionByName(string username)
+    public string? GetConnectionIdByName(string username)
     {
-        _users = new Dictionary<string, string>();
-        return _users[username];
+
+        return _users.TryGetValue(username, out var connectionId) ? connectionId : null;
+
     }
 
     public IEnumerable<(string ConnectionId, string Username)> GetAll()
     {
-        return _users.Select(e => (e.Key, e.Value));
+        foreach (var user in _users)
+        {
+            yield return (user.Value, user.Key);
+        }
     }
 }
+
